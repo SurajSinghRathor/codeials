d3c80bde31b7cc71cff1c1c1315dd57bf59587e6
suraj <mesurajsingh2000@gmail.com>
2023-03-25 02:25:05 +0530

    set up for manual authentication

--- controller/Users_controller.js
+++ controller/Users_controller.js
@@ -12,48 +12,19 @@ module.exports.signup = function(req , res){
    res.render('signup');
 }
 
+//Sign up page mechanism
 module.exports.create = function(req , res){
     ///user/create post request will come here
     
-    if(req.body.password != req.body.cpassword)
-    {
-        return res.redirect("back");
-    }
-    //creating user
-    user.create({
-        email : req.body.email,
-        password : req.body.password,
-        name : req.body.name
-    } ) 
-    .then((newuser) => {
-        res.render("signin");
-        console.log('User created successfully:', newuser);
-      })
-      .catch((err) => {
-        console.error('Error creating user:', err);
-      });
 
       
 }
 
+
+//login page mechanism
 module.exports.CreateSession = function(req , res){
    //signin data be post here
-     user.find(
-    {
-
-       $and : [
-        {email : req.body.email},
-        { password : req.body.password}
-       ]
-    }
-
-        ).then((users) => {
-            console.log("user found");
-            res.render("success");
-        })
-        .catch(() => {
-            console.log("error ");
-        })
+    
 
 
 }
